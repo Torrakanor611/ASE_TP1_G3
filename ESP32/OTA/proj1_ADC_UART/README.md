eb93300f77cd6e4c3f05306fa0c0d1e1f83bdc6e
Pedro Abreu <pedro.abreu@ua.pt>
2022-04-15 15:25:48 +0100

    new try

--- ESP32/OTA/proj1_ADC_UART/README.md
+++ ESP32/OTA/proj1_ADC_UART/README.md
@@ -1,16 +1,9 @@
-## Status: TESTED SUCCESSFULLY
+# Simple OTA example
 
-## Description:
-São configuradas a uart1 e a uart2.
-É configurado a adc1 channel 6 (pino 34). Alterou-se o valor de atenuação da adc para a macro ADC_ATTEN_DB_11 para alcançar uma maior intervalo de voltagem
+This example is based on `esp_https_ota` component's APIs.
 
-Com o FreeRTOS são criadas as tarefas uart_task1 e uart_task2.
+## Configuration
 
-Na 1a tarefa é lido da adc o valor da voltagem com multisampling(x64) e é enviado para a uart2 pela uart1.
+Refer README.md in the parent directory for setup details.
 
-Na 2a tarefa é lido do buffer de receção da uart2 o valor da voltagem enviado pela 1a tarefa e comparado ao último valor recebido, imprimindo no terminal se a voltagem é maior ou menor que o valor anterior. 
-
-### recursos
-código de colegas das semanas anteriores.
-
-exemplo de single read adc [https://github.com/espressif/esp-idf/tree/c18a72134d649aa1985b9e43f8e98198c4eaebf8/examples/peripherals/adc/single_read/single_read](https://github.com/espressif/esp-idf/tree/c18a72134d649aa1985b9e43f8e98198c4eaebf8/examples/peripherals/adc/single_read/single_read)
+Example also supports binding to specific interface (either "Ethernet" or "WiFi Station"), which will allow firmware upgrade to happen through specific interface (in case multiple networking interfaces are enabled). Please see more on this through example configuration in `idf.py menuconfig -> Example Configuration -> Support firmware upgrade bind specificied interface->Choose OTA data bind interface`.
--- ESP32/OTA/proj1_ADC_UART/main/uart_adc_main.c
+++ ESP32/OTA/proj1_ADC_UART/main/uart_adc_main.c
@@ -1,15 +1,11 @@
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/uart.h"
-#include "driver/gpio.h"
-#include "sdkconfig.h"
-#include "esp_rom_gpio.h"
-#include "soc/uart_periph.h"
-#include "driver/gpio.h"
-#include "driver/adc.h"
-#include "esp_adc_cal.h"
+/* OTA example
 
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
@@ -32,6 +28,16 @@
 #include "esp_wifi.h"
 #endif
 
+#include <stdio.h>
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_rom_gpio.h"
+#include "soc/uart_periph.h"
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+
 #define HASH_LEN 32
 
 #ifdef CONFIG_EXAMPLE_FIRMWARE_UPGRADE_BIND_IF
@@ -48,6 +54,26 @@ extern const uint8_t server_cert_pem_start[] asm("_binary_ca_cert_pem_start");
 extern const uint8_t server_cert_pem_end[] asm("_binary_ca_cert_pem_end");
 
 #define OTA_URL_SIZE 256
+
+#define ECHO_UART_BAUD_RATE     115200
+#define BUF_SIZE (1024)
+
+#define UART1_GPIO_RX     (4)
+#define UART1_GPIO_TX     (5)
+
+#define TURN_OFF_GPIO     (36)
+
+const char xOn[] = {0x11, '\0'};
+const char xOff[] = {0x13, '\0'};
+
+#define DEFAULT_VREF    1100       //Use adc2_vref_to_gpio() to obtain a better estimate
+#define NO_OF_SAMPLES   64          //Multisampling
+
+static esp_adc_cal_characteristics_t *adc_chars;
+static const adc_channel_t channel = ADC_CHANNEL_6;     // adc1 channel6 é o pino 34
+static const adc_bits_width_t width = ADC_WIDTH_BIT_12;
+static const adc_atten_t atten = ADC_ATTEN_DB_11;
+static const adc_unit_t unit = ADC_UNIT_1;
 
 esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 {
@@ -79,26 +105,6 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
     }
     return ESP_OK;
 }
-
-#define ECHO_UART_BAUD_RATE     115200
-#define BUF_SIZE (1024)
-
-#define UART1_GPIO_RX     (4)
-#define UART1_GPIO_TX     (5)
-
-#define TURN_OFF_GPIO     (36)
-
-const char xOn[] = {0x11, '\0'};
-const char xOff[] = {0x13, '\0'};
-
-#define DEFAULT_VREF    1100       //Use adc2_vref_to_gpio() to obtain a better estimate
-#define NO_OF_SAMPLES   64          //Multisampling
-
-static esp_adc_cal_characteristics_t *adc_chars;
-static const adc_channel_t channel = ADC_CHANNEL_6;     // adc1 channel6 é o pino 34
-static const adc_bits_width_t width = ADC_WIDTH_BIT_12;
-static const adc_atten_t atten = ADC_ATTEN_DB_11;
-static const adc_unit_t unit = ADC_UNIT_1;
 
 static void check_efuse(void)
 {
@@ -183,6 +189,66 @@ char* convertIntegerToChar(u_int32_t N)
 
 	// Return char array
 	return (char*)arr;
+}
+
+void ota_firmware_update(void *pvParameter)
+{
+    ESP_LOGI(TAG, "Starting OTA example");
+#ifdef CONFIG_EXAMPLE_FIRMWARE_UPGRADE_BIND_IF
+    esp_netif_t *netif = get_example_netif_from_desc(bind_interface_name);
+    if (netif == NULL) {
+        ESP_LOGE(TAG, "Can't find netif from interface description");
+        abort();
+    }
+    struct ifreq ifr;
+    esp_netif_get_netif_impl_name(netif, ifr.ifr_name);
+    ESP_LOGI(TAG, "Bind interface name is %s", ifr.ifr_name);
+#endif
+    esp_http_client_config_t config = {
+        .url = CONFIG_EXAMPLE_FIRMWARE_UPGRADE_URL,
+#ifdef CONFIG_EXAMPLE_USE_CERT_BUNDLE
+        .crt_bundle_attach = esp_crt_bundle_attach,
+#else
+        .cert_pem = (char *)server_cert_pem_start,
+#endif /* CONFIG_EXAMPLE_USE_CERT_BUNDLE */
+        .event_handler = _http_event_handler,
+        .keep_alive_enable = true,
+#ifdef CONFIG_EXAMPLE_FIRMWARE_UPGRADE_BIND_IF
+        .if_name = &ifr,
+#endif
+    };
+
+#ifdef CONFIG_EXAMPLE_FIRMWARE_UPGRADE_URL_FROM_STDIN
+    char url_buf[OTA_URL_SIZE];
+    if (strcmp(config.url, "FROM_STDIN") == 0) {
+        example_configure_stdin_stdout();
+        fgets(url_buf, OTA_URL_SIZE, stdin);
+        int len = strlen(url_buf);
+        url_buf[len - 1] = '\0';
+        config.url = url_buf;
+    } else {
+        ESP_LOGE(TAG, "Configuration mismatch: wrong firmware upgrade image url");
+        abort();
+    }
+#endif
+
+#ifdef CONFIG_EXAMPLE_SKIP_COMMON_NAME_CHECK
+    config.skip_cert_common_name_check = true;
+#endif
+
+    esp_https_ota_config_t ota_config = {
+        .http_config = &config,
+    };
+    ESP_LOGI(TAG, "Attempting to download update from %s", config.url);
+    esp_err_t ret = esp_https_ota(&ota_config);
+    if (ret == ESP_OK) {
+        esp_restart();
+    } else {
+        ESP_LOGE(TAG, "Firmware upgrade failed");
+    }
+    while (1) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
 
 static void print_sha256(const uint8_t *image_hash, const char *label)
@@ -277,72 +343,39 @@ static void uart2_task(void *arg)
     }
 }
 
-void simple_ota_example_task(void *pvParameter)
-{
-    ESP_LOGI(TAG, "Starting OTA firmware update...");
-#ifdef CONFIG_EXAMPLE_FIRMWARE_UPGRADE_BIND_IF
-    esp_netif_t *netif = get_example_netif_from_desc(bind_interface_name);
-    if (netif == NULL) {
-        ESP_LOGE(TAG, "Can't find netif from interface description");
-        abort();
-    }
-    struct ifreq ifr;
-    esp_netif_get_netif_impl_name(netif, ifr.ifr_name);
-    ESP_LOGI(TAG, "Bind interface name is %s", ifr.ifr_name);
-#endif
-    esp_http_client_config_t config = {
-        .url = "https://192.168.1.100:8070/proj2_I2C_PWM.bin",
-#ifdef CONFIG_EXAMPLE_USE_CERT_BUNDLE
-        .crt_bundle_attach = esp_crt_bundle_attach,
-#else
-        .cert_pem = (char *)server_cert_pem_start,
-#endif /* CONFIG_EXAMPLE_USE_CERT_BUNDLE */
-        .event_handler = _http_event_handler,
-        .keep_alive_enable = true,
-#ifdef CONFIG_EXAMPLE_FIRMWARE_UPGRADE_BIND_IF
-        .if_name = &ifr,
-#endif
-    };
-
-#ifdef CONFIG_EXAMPLE_FIRMWARE_UPGRADE_URL_FROM_STDIN
-    char url_buf[OTA_URL_SIZE];
-    if (strcmp(config.url, "FROM_STDIN") == 0) {
-        example_configure_stdin_stdout();
-        fgets(url_buf, OTA_URL_SIZE, stdin);
-        int len = strlen(url_buf);
-        url_buf[len - 1] = '\0';
-        config.url = url_buf;
-    } else {
-        ESP_LOGE(TAG, "Configuration mismatch: wrong firmware upgrade image url");
-        abort();
-    }
-#endif
-
-#ifdef CONFIG_EXAMPLE_SKIP_COMMON_NAME_CHECK
-    config.skip_cert_common_name_check = true;
-#endif
-
-    esp_https_ota_config_t ota_config = {
-        .http_config = &config,
-    };
-    ESP_LOGI(TAG, "Attempting to download update from %s", config.url);
-    esp_err_t ret = esp_https_ota(&ota_config);
-    if (ret == ESP_OK) {
-        esp_restart();
-    } else {
-        ESP_LOGE(TAG, "Firmware upgrade failed");
-    }
-    while (1) {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}
-
-
-
-
 void app_main(void)
 {
-    /* Configure parameters of an UART driver,
+    // Initialize NVS.
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        // 1.OTA app partition table has a smaller NVS partition size than the non-OTA
+        // partition table. This size mismatch may cause NVS initialization to fail.
+        // 2.NVS partition contains data in new format and cannot be recognized by this version of code.
+        // If this happens, we erase NVS partition and initialize NVS again.
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+
+    get_sha256_of_partitions();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+#if CONFIG_EXAMPLE_CONNECT_WIFI
+    /* Ensure to disable any WiFi power save mode, this allows best throughput
+     * and hence timings for overall OTA operation.
+     */
+    esp_wifi_set_ps(WIFI_PS_NONE);
+#endif // CONFIG_EXAMPLE_CONNECT_WIFI
+
+/* Configure parameters of an UART driver,
      * communication pins and install the driver */
     uart_config_t uart_config = {
         .baud_rate = ECHO_UART_BAUD_RATE,
@@ -423,51 +456,12 @@ void app_main(void)
     esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, width, DEFAULT_VREF, adc_chars);
     print_char_val_type(val_type);
 
-    /**
-     * Configure OTA 
-     */
-
-    // Initialize NVS.
-    esp_err_t err = nvs_flash_init();
-    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        // 1.OTA app partition table has a smaller NVS partition size than the non-OTA
-        // partition table. This size mismatch may cause NVS initialization to fail.
-        // 2.NVS partition contains data in new format and cannot be recognized by this version of code.
-        // If this happens, we erase NVS partition and initialize NVS again.
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        err = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(err);
-
-    get_sha256_of_partitions();
-
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
-    ESP_ERROR_CHECK(example_connect());
-
-#if CONFIG_EXAMPLE_CONNECT_WIFI
-    /* Ensure to disable any WiFi power save mode, this allows best throughput
-     * and hence timings for overall OTA operation.
-     */
-    esp_wifi_set_ps(WIFI_PS_NONE);
-#endif // CONFIG_EXAMPLE_CONNECT_WIFI
-
     // task 1 retrieve adc value and send to uart2
     xTaskCreate(uart1_task, "uart1_task", 2048, NULL, 10, NULL);
-
-    //sleep for 50 ms
-    vTaskDelay(50 / portTICK_PERIOD_MS);
 
     // task 2 receive adc value and prints if higher or lower than last value
     xTaskCreate(uart2_task, "uart2_task", 2048, NULL, 20, NULL);
 
-    //sleep for 1 min
-    vTaskDelay(1000*60 / portTICK_PERIOD_MS);
 
     xTaskCreate(&ota_firmware_update, "ota_firmware_update", 8192, NULL, 5, NULL);
 }
